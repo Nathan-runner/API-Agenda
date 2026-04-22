c30c482c9ee43230cfe41272ba98fb498bacc7b0
Devezaa7 <devezaa7@users.noreply.github.com>
2026-04-21 23:08:17 -0300

    feat: CRUD funcionários, serviços e autenticação

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+import 'dotenv/config';
 import express from 'express';
+import authRoutes from './lib/src/routes/authRoutes.js';
+import funcionarioRoutes from './lib/src/routes/funcionarioRoutes.js';
+import servicoRoutes from './lib/src/routes/servicoRoutes.js';
 
 const app = express();
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Servidor rodando com sucesso!');
-});
+app.use('/auth', authRoutes);
+app.use('/funcionarios', funcionarioRoutes);
+app.use('/servicos', servicoRoutes);
+
+app.get('/', (req, res) => res.send('Servidor rodando!'));
 
 export default app;
